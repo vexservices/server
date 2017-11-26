b84e73bf170bf6fa797ad0ac40930d2f01bbf672
Nancy Piedra <nancy_piedra@yahoo.com>
2017-11-26 12:21:56 +0000

    Change server name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'vex.phowma.com', user: 'ubuntu', roles: %w{web app db}
+server 'www.virtualopenexchange.com', user: 'ubuntu', roles: %w{web app db}
 
 
 # role-based syntax
